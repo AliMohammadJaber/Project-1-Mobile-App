041fb21b70ba4a96cca253b352efb38655b6dc84
AliMohammadJaber <42210031@students.liu.edu.lb>
2023-11-22 16:28:57 +0200

    Update 2

---
+++ lib/game_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class RockPaperScissorsGame extends StatefulWidget {
+  const RockPaperScissorsGame({Key? key}) : super(key: key);
+
+  @override
+  RockPaperScissorsGameState createState() => RockPaperScissorsGameState();
+}
+
+class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
+  final List<String> choices = ['rock', 'paper', 'scissors'];
+  String userChoice = '';
+  String computerChoice = '';
+  String result = '';
+
+  void makeComputerChoice() {
+    final Random random = Random();
+    int index = random.nextInt(3);
+    computerChoice = choices[index];
+  }
+
+  void playGame(String userChoice) {
+    makeComputerChoice();
+    setState(() {
+      this.userChoice = userChoice;
+      if (userChoice == computerChoice) {
+        result = 'It\'s a tie!';
+      } else if (
+      (userChoice == 'rock' && computerChoice == 'scissors') ||
+          (userChoice == 'paper' && computerChoice == 'rock') ||
+          (userChoice == 'scissors' && computerChoice == 'paper')) {
+        result = 'You win!';
+      } else {
+        result = 'You lose!';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rock, Paper, Scissors'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Choose your move:',
+            style: TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: choices.map((choice) {
+              return ElevatedButton(
+                onPressed: () => playGame(choice),
+                child: Image.asset(
+                  'assets/$choice.png',
+                  height: 80,
+                  width: 80,
+                ),
+              );
+            }).toList(),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Your Choice: $userChoice',
+            style: TextStyle(fontSize: 18),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Computer\'s Choice: $computerChoice',
+            style: TextStyle(fontSize: 18),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            '   Result: \n $result',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,44 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'game_page.dart';
 
 void main() {
-  runApp(const RockPaperScissorsApp());
-
+  runApp(const MyApp());
 }
 
-class RockPaperScissorsApp extends StatelessWidget {
-  const RockPaperScissorsApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-        centerTitle: true,
-          title: const Text('Rock, Paper, Scissors'),
-        ),
-        body: const RockPaperScissorsGame(),
-      ),
+      initialRoute: '/', // Set the initial route
+      routes: {
+        '/': (context) => const HomeScreen(), // Home screen with the Start Game button
+        '/game': (context) => RockPaperScissorsGame(), // Game screen
+      },
     );
   }
 }
 
-class RockPaperScissorsGame extends StatefulWidget {
-  const RockPaperScissorsGame({super.key});
-
-  @override
-  RockPaperScissorsGameState createState() => RockPaperScissorsGameState();
-}
-
-class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
-  final List<String> choices = ['rock', 'paper', 'scissors'];
-  String userChoice = '';
-  String computerChoice = '';
-  String result = '';
-
-  void makeComputerChoice() {
-    final Random random = Random();
-    int index = random.nextInt(3);
-    computerChoice = choices[index];
-  }
-
-  void playGame(String userChoice) {
-    makeComputerChoice();
-    setState(() {
-      this.userChoice = userChoice;
-      if (userChoice == computerChoice) {
-        result = 'It\'s a tie!';
-      } else if (
-      (userChoice == 'rock' && computerChoice == 'scissors') ||
-          (userChoice == 'paper' && computerChoice == 'rock') ||
-          (userChoice == 'scissors' && computerChoice == 'paper')) {
-        result = 'You win!';
-      } else {
-        result = 'You lose!';
-      }
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    return Column(
-
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-
-        const Text(
-
-          'Choose your move:',
-          style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Rock, Paper, Scissors'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/game'); // Navigate to the game screen
+          },
+          child: const Text('Start Game'),
         ),
-        const SizedBox(height: 20),
-        // Inside the _RockPaperScissorsGameState class
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: choices.map((choice) {
-
-            return GestureDetector(
-              onTap: () => playGame(choice),
-              child: Image.asset(
-                'assets/$choice.png',
-                height: 80,
-                width: 80,
-              ),
-            );
-          }).toList(),
-        ),
-
-        const SizedBox(height: 20),
-        Text(
-          'Your Choice: $userChoice',
-          style: const TextStyle(fontSize: 18),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          'Computer\'s Choice: $computerChoice',
-          style: const TextStyle(fontSize: 18),
-        ),
-        const SizedBox(height: 20),
-        Text(
-          '   Result: \n $result',
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ],
+      ),
     );
   }
 }
