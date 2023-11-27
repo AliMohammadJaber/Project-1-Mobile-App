3a76f22852831be211cfb117d9b909a73e8c97f8
AliMohammadJaber <42210031@students.liu.edu.lb>
2023-11-27 19:02:38 +0200

    Last Commit

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter/services.dart';
+
 class RockPaperScissorsGame extends StatefulWidget {
   const RockPaperScissorsGame({Key? key}) : super(key: key);
 
@@ -13,6 +15,12 @@ class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
   String userChoice = '';
   String computerChoice = '';
   String result = '';
+  int wins = 0;
+  int losses = 0;
+  int ties = 0;
+
+
+
 
   void makeComputerChoice() {
     final Random random = Random();
@@ -26,13 +34,59 @@ class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
       this.userChoice = userChoice;
       if (userChoice == computerChoice) {
         result = "It's a tie!";
+        ties++;
       } else if (
       (userChoice == 'rock' && computerChoice == 'scissors') ||
           (userChoice == 'paper' && computerChoice == 'rock') ||
           (userChoice == 'scissors' && computerChoice == 'paper')) {
         result = 'You win!';
+        wins++;
       } else {
         result = 'You lose!';
+        losses++;
+      }
+      if (losses >= 3) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Center(child: Text('Game Over')),
+              content: Text(
+                'You got 3 losses. Better luck next time!\n\n'
+                    '                        Wins: $wins \u{1F451}\n'
+                    '                        Ties: $ties \u{2694}',
+              ),
+              actions: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        SystemNavigator.pop();
+                      },
+                      child: const Text("Quit Game", style: TextStyle(color: Colors.red)),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        wins = 0;
+                        losses = 0;
+                        ties = 0;
+                        Navigator.popUntil(context, ModalRoute.withName('/'));
+                      },
+                      child: const Text("Try Again", style: TextStyle(color: Colors.green)),
+                    ),
+
+                  ],
+                ),
+              ],
+            );
+          },
+        ).then((value) {
+          wins = 0;
+          losses = 0;
+          ties = 0;
+          Navigator.popUntil(context, ModalRoute.withName('/'));
+        });
       }
     });
   }
@@ -65,7 +119,12 @@ class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
             children: [
               const Text(
                 'Choose your move:',
-                style: TextStyle(fontSize: 20,color:Colors.white),
+                style: TextStyle(fontSize: 30,color:Colors.white),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                'Your Loses: $losses',
+                style: const TextStyle(fontSize: 20, color: Colors.white),
               ),
               const SizedBox(height: 20),
               Row(
@@ -73,10 +132,13 @@ class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
                 children: choices.map((choice) {
                   return ElevatedButton(
                     onPressed: () => playGame(choice),
+                  style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(100, 100),
+                  ),
                     child: Image.asset(
                       'assets/$choice.png',
-                      height: 80,
-                      width: 80,
+                      height: 100,
+                      width: 100,
                     ),
                   );
                 }).toList(),
@@ -84,12 +146,12 @@ class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
               const SizedBox(height: 20),
               Text(
                 'Your Choice: $userChoice',
-                style: const TextStyle(fontSize: 18 ,color:Colors.white),
+                style: const TextStyle(fontSize: 25 ,color:Colors.white),
               ),
               const SizedBox(height: 10),
               Text(
                 "Computer's Choice: $computerChoice",
-                style: const TextStyle(fontSize: 18 ,color:Colors.white),
+                style: const TextStyle(fontSize: 25 ,color:Colors.white),
               ),
               const SizedBox(height: 20),
               Text(
@@ -98,12 +160,8 @@ class RockPaperScissorsGameState extends State<RockPaperScissorsGame> {
               ),
             ],
           ),
-
         ],
       ),
-
-
-
     );
   }
 }
